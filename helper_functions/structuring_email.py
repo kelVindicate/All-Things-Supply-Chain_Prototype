436614da94e0bea6aac15197f1735be9183daf23
daveathometoday <137516339+daveathometoday@users.noreply.github.com>
2025-08-25 11:41:21 +0000

    Updated imports and cleaned code

--- helper_functions/structuring_email.py
+++ helper_functions/structuring_email.py
@@ -1,3 +1,7 @@
+import os 
+os.environ.setdefault("CHROMA_DB_IMPL", "duckdb+parquet")
+os.environ.setdefault("CREWAI_STORAGE_DIR", ".crewai_storage")
+
 import sys 
 try:
     import pysqlite3
--- pages/1 Supply Chain News Generator.py
+++ pages/1 Supply Chain News Generator.py
@@ -1,3 +1,6 @@
+import os 
+os.environ.setdefault("CHROMA_DB_IMPL", "duckdb+parquet")
+os.environ.setdefault("CREWAI_STORAGE_DIR", ".crewai_storage")
 import sys 
 try:
     import pysqlite3
